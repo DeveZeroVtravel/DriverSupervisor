0baf34796f636db9c9f379181b55f92b653f6202
MAC <mac@Mac.lan>
2025-09-22 20:14:45 +0700

    BETA2

--- TESTSTRUCTURE/Main.py
+++ TESTSTRUCTURE/Main.py
@@ -12,6 +12,8 @@ cv2.namedWindow("DSDS",cv2.WINDOW_NORMAL)
 cv2.setWindowProperty("DSDS",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
 
 fps,frameCount,StartTime=0,0,time.time()
+viewMode=0
+
 
 while True:
     frame=camera.get_frame()
@@ -21,7 +23,8 @@ while True:
     frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     result=faceMesh.process(frame)
     
-    frame=render(frame,result,selectPoint,(0,255,0),1,-1)
+    if viewMode==1:
+        frame=render(frame,result,selectPoint,(0,255,0),1,-1)
 
     cv2.putText(frame, f"FPS:{fps:.2f}",(10,20),cv2.FONT_HERSHEY_SIMPLEX,0.6,(0,255,0),2)
 
@@ -33,6 +36,10 @@ while True:
         fps=frameCount/tc
         frameCount,StartTime=0,time.time()
     
+    if cv2.waitKey(1) & 0xFF == ord('v'):
+        if viewMode==1: viewMode=0
+        else: viewMode=1
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- TESTSTRUCTURE/MyMediapipe.py
+++ TESTSTRUCTURE/MyMediapipe.py
@@ -1,10 +1,13 @@
 import mediapipe as mp
 
+with open("TESTSTRUCTURE/setting.json","r",encoding="utf-8")as f: setting=json.load(f)
+
+
+
 mp_face_mesh=mp.solutions.face_mesh
 faceMesh=mp_face_mesh.FaceMesh(
     static_image_mode=False,
-    max_num_faces=1,
     refine_landmarks=True,
-    min_detection_confidence=0.22,
-    min_tracking_confidence=0.78
+    max_num_faces=setting[mediapipe][detectionConfident],
+    min_tracking_confidence=setting[mediapipe][trackingConfident]
 )
